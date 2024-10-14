1424b63ba6cc742733b25f8336559691448053a8
matvey <1132226162@pfur.ru>
2024-10-14 17:30:08 +0300

    fucking tests

--- src/backend/python/tests/test_example.py
+++ src/backend/python/tests/test_example.py
@@ -4,4 +4,4 @@ import requests
 def test_info_endpoint():
     response = requests.get("http://127.0.0.1:8080/info")
     assert response.status_code == 200
-    assert response.content == "Hello from backend"
+    assert response.content == b'"Hello from backend"'
